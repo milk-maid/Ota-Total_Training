b244a6a8469ddff859a94aa84dd6ad3655e70ccd
Bello Roqeeb <roqbell@gmail.com>
2022-08-01 14:56:36 +0100

    syllabus

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # specializedClass
+
+The **Syllabus** designed for the task ahead at *OTA TOTAL ACADEMY*
+
+## Technologies
+
+* HTML for markup
+* CSS for styling and more
+* No JS files as this is just a casual design
+* README: the markdown file attached to the root of the repo.
+
+## Files
+
+All the following files are programme written during the building of the counter
+| Filename | Description |
+| -------- | ------------ |
+| `index.html` | the markup file that contains the main html code |
+| `style.css` | the linked stylesheet |
+| `README.md` | the markdown file added to the root of the repository |
---
+++ web2.js
@@ -0,0 +1,76 @@
+
+    document.write("<br>");
+
+//Ass QUESTION 2 (20marks)
+
+    let dist = 500;
+    let time = 10;
+    let av_speed;
+    av_speed = dist / time;
+    document.write(
+        'The speed of the car is ' + av_speed + 'km/hr'
+        );
+   
+    document.write("<br>")
+
+//Ass QUESTION 3 (5marks)
+
+    //a=5;
+      a=2;
+    //a=6;
+   document.write(
+       'The output of the code is ' + a + ' because other code lines were commented out.'
+       );
+
+    document.write("<br>")
+    
+// ***TODAY 7/5/2022 CLASSWORK***
+
+   // let mth1 = "June";
+    //  if(mth1 == "August"){
+    //      document.write("I'm ready for vacation");
+    //  } else{
+    //      document.write("...then, I'm not ready");
+    // 
+    // let score = 11;
+    // if(score >= 70 && score <= 100){
+    //     document.write("Excellent");
+    // }
+    // else if (score >= 60 && score <= 69  ){
+    //     document.write("Very Good");
+    // }
+    // else if (score >= 50 && score <= 59){
+    //     document.write("Good");
+    // }
+    // else if (score >= 40 && score <= 49){
+    //     document.write("Fair");
+    // }
+    // else if (score >= 0 && score <= 39 ) {
+    //     document.write("Fail");
+    // }
+    // else {
+    //     document.write("no such grade")
+    // }
+
+
+    //classwork solution ANOTHER STYLE
+
+    // let score = 77;
+    // switch(true){
+    //     case score >= 70:
+    //         document.write("Excellent");
+    //         break;
+    //     case score >= 60 && score <= 69:
+    //         document.write("Very Good");
+    //         break;
+    //     case score >= 50 && score <= 59:
+    //         document.write("Good");
+    //         break;
+    //     case score >= 40 && score <= 49:
+    //         document.write("Fair");
+    //         break;
+    //     case score >= 0 && score <= 39:
+    //         document.write("Fail");
+    //         break;
+    // }
+                
